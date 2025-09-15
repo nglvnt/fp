23b8f538ba764efc4ac874119c8bfc71da1513d3
nglvnt <nglvnt@protonmail.com>
2025-09-15 22:12:40 +0200

    Change path parameter to specific type in basic API

--- fp-haskell/src/webapis/api.hs
+++ fp-haskell/src/webapis/api.hs
@@ -6,11 +6,12 @@ import GHC.Generics
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Handler.Warp (run)
+import Text.Read
 
 newtype Message = Message {message :: Text} deriving (Show, Generic)
 instance ToJSON Message
 
-newtype Item = Item {itemId :: Text} deriving (Show, Generic)
+newtype Item = Item {itemId :: Int} deriving (Show, Generic)
 instance ToJSON Item
 
 -- run server
@@ -23,7 +24,12 @@ main = do
 app :: Application
 app request respond = respond $ case (requestMethod request, pathInfo request) of
     ("GET", []) -> root
-    ("GET", ["item", itemId]) -> getItem itemId
+    ("GET", ["item", itemId]) -> case parseItemId itemId of
+        Left _ -> validationError
+        Right parsedItemId -> getItem parsedItemId
+
+parseItemId :: Text -> Either String Int
+parseItemId text = readEither $ unpack text
 
 -- get root function
 root :: Response
@@ -32,8 +38,14 @@ root = responseLBS
     [(hContentType, "application/json")]
     (encode Message {message = "Hello World!"})
 
+validationError :: Response
+validationError = responseLBS
+    status422
+    [(hContentType, "application/json")]
+    (encode Message {message = "Validation error"})
+
 -- get item by ID
-getItem :: Text -> Response
+getItem :: Int -> Response
 getItem item = responseLBS
     status200
     [(hContentType, "application/json")]
